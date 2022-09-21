8f62b32706dbc553f15c5fa074e2ebfe9ac6db3e
AlcorsoLain <114010965+AlcorsoLain@users.noreply.github.com>
2022-09-20 23:54:16 -0500

    Base 0

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hola mundo")
+import tkinter as tk
+from tkinter import ttk
+
+#clase contr0ls, declara los controles
+
+class contr0ls(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.Label1 = ttk.Label(parent, text="Bienvenido a PYtilidades")
+        self.Label1.place(x=20,y=20)
+        self.Btn_Calculator = ttk.Button(parent, text="Calculadora")
+        self.Btn_Calculator.place(x=30,y=50)
+
+
+#Iniciacion de Tkinter
+main = tk.Tk()
+main.title("PYtilidades")
+main.geometry("400x300")
+app = contr0ls(main)
+main.mainloop()
