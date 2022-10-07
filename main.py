97ad72a510ce1cf9e81c68814c4815d4044ed2d9
AlcorsoLain <114010965+AlcorsoLain@users.noreply.github.com>
2022-10-06 23:37:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-import tkinter as tk
-from tkinter import ttk
-
-#clase contr0ls, declara los controles
-
-class contr0ls(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        self.Label1 = ttk.Label(parent, text="Bienvenido a PYtilidades")
-        self.Label1.place(x=20,y=20)
-        self.Btn_Calculator = ttk.Button(parent, text="Calculadora")
-        self.Btn_Calculator.place(x=30,y=50)
-
-
-#Iniciacion de Tkinter
-main = tk.Tk()
-main.title("PYtilidades")
-main.geometry("400x300")
-app = contr0ls(main)
-main.mainloop()
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter.ttk import *
+from Clases.calculadora import app
+#icon
+root = Tk()
+root.title("-----")
+root.geometry("425x508")
+root.mainloop()
+# stuff you want to do
+root.after(10000, app())
