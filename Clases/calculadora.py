a84bc66b57b9ec37f112a169bda8994fa6ac54a9
AlcorsoLain <114010965+AlcorsoLain@users.noreply.github.com>
2022-10-06 23:37:32 -0500

    HOTFIX

---
+++ Clases/calculadora.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import ttk
+# icon
+def app():
+    root = Tk()
+    root.title("selection")
+    root.geometry("325x396")
+    lar = ttk.Label(root,text="Hola mundo")
+    root.mainloop()
